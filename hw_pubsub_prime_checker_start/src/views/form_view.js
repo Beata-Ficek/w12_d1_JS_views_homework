2bff936be7d41448063cdc3bca2fc040915f043b
Bea <beata.ficek@gmail.com>
2018-09-11 08:51:02 +0100

    refactor

--- hw_pubsub_prime_checker_start/src/views/form_view.js
+++ hw_pubsub_prime_checker_start/src/views/form_view.js
@@ -16,23 +16,3 @@ FormView.prototype.bindEvents = function(){
 }
 
 module.exports = FormView;
-
-
-// const PubSub = require('../helpers/pub_sub')
-//
-// const FormView = function(){
-//
-// }
-//
-// FormView.prototype.bindEvents = function(){
-//
-//   const form = document.querySelector("#wordcounter-form");
-//   form.addEventListener('submit', (event) => {
-//     event.preventDefault();
-//     const typedText = event.target.text.value;
-//     // console.log("typedText:"+typedText);
-//     PubSub.publish('FormView:text-submitted', typedText);
-//   })
-// }
-//
-// module.exports = FormView;
